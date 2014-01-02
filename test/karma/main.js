b32e14284c2030cfc201c0c69db98269e112694e
James Morrin <treasonx@gmail.com>
2014-01-02 14:00:15 -0500

    clean up

--- test/karma/main.js
+++ test/karma/main.js
@@ -1,21 +1,24 @@
-var allTestFiles = [];
-var TEST_REGEXP = /.+Spec.js$/;
+(function(window) {
+  'use strict';
+  var allTestFiles = [];
+  var TEST_REGEXP = /.+Spec.js$/;
 
-Object.keys(window.__karma__.files).forEach(function(file) {
-    if (TEST_REGEXP.test(file)) {
-        allTestFiles.push(file);
-    }
-});
+  Object.keys(window.__karma__.files).forEach(function(file) {
+      if (TEST_REGEXP.test(file)) {
+          allTestFiles.push(file);
+      }
+  });
 
-window.dojoConfig = {
-    packages: [
-        { name:"app" ,location:"/base/web_client/app"},
-        { name: "dojo", location: "/base/web_client/libs/dojo" },
-        { name: "dijit", location: "/base/web_client/libs/dijit" }
-    ],
-    asynch: true
-};
+  window.dojoConfig = {
+      packages: [
+          { name: 'app' ,location:'/base/web_client/app'},
+          { name: 'dojo', location: '/base/web_client/libs/dojo' },
+          { name: 'dijit', location: '/base/web_client/libs/dijit' }
+      ],
+      asynch: true
+  };
 
-window.__karma__.dojoStart = function(){
-    return allTestFiles;
-}
+  window.__karma__.dojoStart = function(){
+      return allTestFiles;
+  };
+}(this));
