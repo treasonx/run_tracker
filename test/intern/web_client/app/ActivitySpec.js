6000d53c24bfe81c317ad433fd513d360f544349
James Morrin <treasonx@gmail.com>
2014-01-02 14:45:15 -0500

    add static rendering specs

--- test/intern/web_client/app/ActivitySpec.js
+++ test/intern/web_client/app/ActivitySpec.js
@@ -1,20 +1,63 @@
 define([
   'intern!bdd',
   'intern/chai!expect',
-  'app/Activity'
+  'app/Activity',
+  'dojo/dom-construct'
 ], function(
   b,
   expect,
-  Activity
+  Activity,
+  domConstruct
 ) {
   'use strict';
 
-  b.describe('Activity', function() {
+  b.describe('activity', function() {
+
+    var data = {
+      start_time: 'expected time',
+      duration: 234455,
+      total_distance: 345985.34
+    };
+
+    var ele = null;
+    var activity = null;
+
+    b.beforeEach(function() {
+      if(activity) {
+        activity.destroy();
+      }
+      if(ele) {
+        domConstruct.destroy(ele);
+      }
+      ele = document.createElement('div');
+      domConstruct.place(ele, document.body);
+    });
+
+
     b.it('should be sane', function() {
       expect(Activity).to.be.ok;
     });
+
+    b.describe('static rendering', function() {
+      b.beforeEach(function() {
+        activity = new Activity(data, ele);
+        activity.startup();
+      });
+
+      b.it('should render start time', function() {
+        expect(activity.domNode.innerText).to.have.string(data.start_time);
+      });
+
+      b.it('should render duration', function() {
+        expect(activity.domNode.innerText).to.have.string(data.duration);
+      });
+
+      b.it('should render total distance', function() {
+        expect(activity.domNode.innerText).to.have.string(data.total_distance);
+      });
+
+    });
+
   });
-
-
 
 });
--- test/karma/web_client/app/ActivitySpec.js
+++ test/karma/web_client/app/ActivitySpec.js
@@ -1,8 +1,64 @@
-define(['app/Activity'], function(Activity) {
+define([
+  'app/Activity',
+  'dojo/dom-construct'
+], function(
+  Activity,
+  domConstruct
+) {
+  'use strict';
+
 
   describe('activity', function() {
+
+    var data = {
+      start_time: 'expected time',
+      duration: 234455,
+      total_distance: 345985.34
+    };
+
+    var ele = null;
+    var activity = null;
+
+    beforeEach(function() {
+      if(activity) {
+        activity.destroy();
+      }
+      if(ele) {
+        domConstruct.destroy(ele);
+      }
+      ele = document.createElement('div');
+      domConstruct.place(ele, document.body);
+    });
+
     it('should be sane', function() {
       expect(Activity).toBeTruthy();
     });
+
+    describe('static rendering', function() {
+      beforeEach(function() {
+        activity = new Activity(data, ele);
+        activity.startup();
+      });
+
+      it('should render start time', function() {
+        expect(activity.domNode.innerText).toContain(data.start_time);
+      });
+
+      it('should render duration', function() {
+        expect(activity.domNode.innerText).toContain(data.duration);
+      });
+
+      it('should render total distance', function() {
+        expect(activity.domNode.innerText).toContain(data.total_distance);
+      });
+
+
+
+    });
+
+
+
+
+
   });
 });
